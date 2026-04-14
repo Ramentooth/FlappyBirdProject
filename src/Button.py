79232f316319d27367dc0caa86d5d27d3c07f78c
Ramentooth <Ramentooth@gmail.com>
2026-04-14 11:48:49 -0600

    Refactor Buttons class initialization and drawButton method

--- src/Button.py
+++ src/Button.py
@@ -1,19 +1,37 @@
 import pygame
+from Settings import screenH, screenL, speed
 
 class Buttons:
-    def __init__(self,x,y,w,h,cM,cT,cS):
+    def __init__(self,x,y,w,h,cMain,cDown): #cText,cShade
         self.x = x
         self.y = y
         self.w = w
         self.h = h
-        self.cM = cM
-        self.cT = cT
-        self.cS = cS
+        self.cMain = cMain
+        self.cDown = cDown
+        self.clickedVal = False
+        self.display = True
+        #self.cText = cText
+        #self.cSshade = cShade
 
-    def drawButton(self):
+    def drawButton(self, screen, mousX, mousY, MOUSEBUTTONUP, MOUSEBUTTONDOWN, otherStartVal):
+        if self.display:
+            if (self.x - self.w/2) <= mousX <= ((self.x - self.w/2) + self.w) and (self.y - self.h/2) <= mousY <= ((self.y - self.h/2) + self.h):
+                pygame.draw.rect(screen, self.cDown, ((self.x - self.w/2), (self.y - self.h/2), self.w, self.h), 0, 20)
+                if MOUSEBUTTONDOWN and MOUSEBUTTONUP != True:
+                    self.clickedVal = True
+                    self.display = False
+            elif otherStartVal:
+                self.clickedVal = True
+                self.display = False
+            else:
+                pygame.draw.rect(screen, self.cMain, ((self.x - self.w/2), (self.y - self.h/2), self.w, self.h), 0, 20)
+            
+    def gravity():
         pass
-    def buttonClicked(self):
-        pass
-    
+        
+
+        
+
 
         
