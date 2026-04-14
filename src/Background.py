3f60c6294949bb21c4e5a6d28c58f12b6e4ff560
Ramentooth <Ramentooth@gmail.com>
2026-04-14 11:54:00 -0600

    Update Background.py

--- src/Background.py
+++ src/Background.py
@@ -1,6 +1,8 @@
 import pygame
+from Settings import screenH, speed
 
 class Backgrounds:
     def __init__(self,screenMoving,photo):
         self.screenMoving = screenMoving
         self.photo = photo
+    def displayBackgound(self):
