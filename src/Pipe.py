8f41ef1805a8469aa5c2d1cc5cf9a27e22d0a0f5
Ramentooth <Ramentooth@gmail.com>
2026-04-14 11:49:31 -0600

    Refactor Pipes class and implement pipe movement
    Refactor Pipes class to Pipe class and simplify initialization. Add space generation and pipe movement logic.

--- src/Pipe.py
+++ src/Pipe.py
@@ -1,13 +1,29 @@
 import pygame
+import random
+from Settings import screenH, screenL, speed, gap, pipeW, rimW, rimL 
 
-class Pipes:
-    def __init__(self,x,y,w,h,type):
+class Pipe:
+    def __init__(self,x,y,type):
         self.x = x
         self.y = y
-        self.w = w
-        self.h = h
         self.type = type
+        self.spaceGen = random.randint(50,450)
     def movePipes(self):
-        pass
+        self.x -= speed
+        if self.x == -pipeW:
+            self.x += screenL + 100
+            self.spaceGen = random.randint(50,450)
+
+
+
     def displayPipes(self):
         pass
+    def makePipes(self):
+        topL = self.spaceGen
+        bottomY = topL + gap 
+        bottomL = screenH - bottomY 
+        top = (self.x, 0, pipeW, topL)
+        topRim = (self.x - rimW, topL - rimL, pipeW + rimW + rimW, rimL)
+        bottom = (self.x, bottomY, pipeW, bottomL)
+        bottomRim = (self.x - rimW, bottomY, pipeW + rimW + rimW, rimL)
+        yield top, bottom, topRim, bottomRim
