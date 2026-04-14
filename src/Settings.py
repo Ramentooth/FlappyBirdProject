f4a7985c9f4e1196aad7142cc1dca9c36293ae9f
Ramentooth <Ramentooth@gmail.com>
2026-04-14 11:51:07 -0600

    Add Settings.py for game configuration variables

---
+++ src/Settings.py
@@ -0,0 +1,17 @@
+import pygame
+
+# SCREEN VARIABLES
+screenH = 700
+screenL = 1500
+screen = pygame.display.set_mode((screenL, screenH))
+
+# GAMEPLAY VARIABLES
+speed = 6
+gap = 150
+pipeW = 100
+
+# PIPE DESIGN
+rimW = 10
+rimL = 30
+pipeC = (0, 255, 100)
+
