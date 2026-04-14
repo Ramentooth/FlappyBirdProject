7e02019f6932eafbac32206a5de459192b51768f
Ramentooth <Ramentooth@gmail.com>
2026-04-14 11:48:26 -0600

    Refactor Birds class to Bird and add gravity method

--- src/Bird.py
+++ src/Bird.py
@@ -1,18 +1,26 @@
 import pygame
 
-class Birds:
+class Bird:
     def __init__(self,x,y,w,h,type):
         self.x = x
         self.y = y
         self.w = w
         self.h = h
         self.type = type
+        self.velocity = 0
+        self.GRAVITY = 0.02
+        self.BOOST = -25
 
-    def gravity(self):
-        pass
-    
-    def crash(self):
-        pass
+    def drawBird(self):
+        bird = (self.x - 50, self.y - 50, self.w, self.h)
+        return bird
+
+    def gravity(self,SPACECLICKED):
+        if SPACECLICKED:
+            self.y += self.BOOST
+            self.velocity = 0
+        self.y += self.velocity
+        self.velocity += self.GRAVITY
         
     def birdType(self):
         pass
