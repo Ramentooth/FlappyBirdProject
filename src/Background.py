37e37dc7c38e17456d89f30662ea728b3da03ef4
Ramentooth <Ramentooth@gmail.com>
2026-04-21 22:19:38 -0600

    Refactor Background class and methods
    Refactor Background class to include x and y coordinates and update display method.

--- src/Background.py
+++ src/Background.py
@@ -1,8 +1,28 @@
 import pygame
-from Settings import screenH, speed
+from Settings import speed, screenL, screenH
 
-class Backgrounds:
-    def __init__(self,screenMoving,photo):
-        self.screenMoving = screenMoving
+class Background:
+    def __init__(self,x,y,photo):
+        self.x = x
+        self.y = y
+        self.x2 = x + screenL
         self.photo = photo
-    def displayBackgound(self):
+        self.photoSpeed = speed
+        self.bg = pygame.image.load(self.photo).convert()
+        self.Sbg = pygame.transform.scale(self.bg, (screenL, screenH))
+        self.Sbg2 = pygame.transform.scale(self.bg, (screenL, screenH))
+    def moveBG(self):
+        self.x -= speed/1.5
+        self.x2 -= speed/1.5
+        if self.x <= -screenL:
+            self.x = screenL
+        if self.x2 <= -screenL:
+            self.x2 = screenL
+    def startScreen():
+        pass
+    def endScreen():
+        pass
+
+
+        
+    
