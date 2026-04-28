43371594eda4351b4c17727e2a3c0ccaab2f06d3
Ramentooth <Ramentooth@gmail.com>
2026-04-28 12:40:09 -0600

    Refactor Button class to include new color parameters

--- src/Button.py
+++ src/Button.py
@@ -2,7 +2,7 @@ import pygame
 from Settings import screenH, screenL, speed, font
 
 class Buttons:
-    def __init__(self,x,y,w,h,cMain,cDown,text): #cText,cShade
+    def __init__(self,x,y,w,h,cMain,cDown,text,cText,cButton,cButtonDown): #cText,cShade
         self.x = x
         self.y = y
         self.w = w
@@ -12,24 +12,27 @@ class Buttons:
         self.text = text
         self.clickedVal = False
         self.display = True
-        #self.cText = cText
+        self.cText = cText
+        self.cButton = cButton
+        self.cButtonDown = cButtonDown
         #self.cSshade = cShade
 
-    def drawButton(self, screen, mousX, mousY, MOUSEBUTTONUP, MOUSEBUTTONDOWN, otherStartVal):
+    def drawButton(self, screen, mousX, mousY, MOUSEBUTTONUP, MOUSEBUTTONDOWN, SPACECLICKED):
         if self.display:
             if (self.x - self.w/2) <= mousX <= ((self.x - self.w/2) + self.w) and (self.y - self.h/2) <= mousY <= ((self.y - self.h/2) + self.h):
-                pygame.draw.rect(screen, self.cDown, ((self.x - self.w/2), (self.y - self.h/2), self.w, self.h), 0, 20)
+                pygame.draw.rect(screen, self.cDown, ((self.x - self.w/2), (self.y - self.h/2), self.w, self.h), self.cButtonDown, 20)
                 Bfont = pygame.font.SysFont(None,64)
-                Btext = Bfont.render(self.text, True, (0))
-                screen.blit(Btext,(self.x,self.y + self.h/4))
-                if MOUSEBUTTONDOWN and MOUSEBUTTONUP != True or otherStartVal:
+                Btext = Bfont.render(self.text, True, (0,0,0), (255,255,255))
+                textRect = Btext.get_rect(center=(self.x,self.y))
+                screen.blit(Btext,textRect)
+                if MOUSEBUTTONDOWN and MOUSEBUTTONUP != True or SPACECLICKED:
                     self.clickedVal = True
                     self.display = False
-            elif otherStartVal:
+            elif SPACECLICKED:
                 self.clickedVal = True
                 self.display = False
             else:
-                pygame.draw.rect(screen, self.cMain, ((self.x - self.w/2), (self.y - self.h/2), self.w, self.h), 0, 20)
+                pygame.draw.rect(screen, self.cMain, ((self.x - self.w/2), (self.y - self.h/2), self.w, self.h), self.cButton, 20)
             
     def gravity():
         pass
