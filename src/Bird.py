d0d458b1b14b578a156e3ad26ca6ac30e80e42da
Ramentooth <Ramentooth@gmail.com>
2026-05-01 21:28:02 -0600

    Refactor Bird class and add sound/image handling

--- src/Bird.py
+++ src/Bird.py
@@ -1,4 +1,5 @@
 import pygame
+pygame.mixer.init()
 
 class Bird:
     def __init__(self,x,y,w,h,type):
@@ -9,21 +10,33 @@ class Bird:
         self.type = type
         self.velocity = 0
         self.GRAVITY = 0.02
-        self.BOOST = -25
+        self.BOOST = 0
+        self.flappySFX = pygame.mixer.Sound('FlappyJump.mp3')
+        self.birdPicUp = pygame.transform.scale(pygame.image.load('birdPicDown.jpg').convert_alpha(), (50, 50))
+        self.birdGlide = pygame.transform.scale(pygame.image.load('birdGlide.jpg').convert_alpha(), (50, 50))
+        #self.unscaledPicDown = Background(self.x, self.y, 'birdPicDown')
+        #self.birdPicDown = Background(self.x, self.y, 'birdPicDown')
 
-    def drawBird(self):
+    def birdPos(self):
         bird = (self.x - 50, self.y - 50, self.w, self.h)
         return bird
 
     def gravity(self,SPACECLICKED):
         if SPACECLICKED:
-            self.y += self.BOOST
+            self.flappySFX.play()
+            self.BOOST = -2.3
             self.velocity = 0
+
         self.y += self.velocity
         self.velocity += self.GRAVITY
+        self.y += self.BOOST
+        self.BOOST += self.GRAVITY
         
     def birdType(self):
         pass
 
-    def controls(self):
-        pass
+    def birdImage(self):
+        if self.velocity < 0:
+            return self.birdPicUp   
+        else:
+            return self.birdGlide
